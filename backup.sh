f5235f91d5bb6c96ee5b328c20456aa96df67bd9
nadhilafiq <67740092+nadhilafiq@users.noreply.github.com>
2023-07-06 21:57:36 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 DATE=$(date +"%Y-%m-%d")
 docker exec db pg_dumpall -c -U appuser > /backup/dump_$DATE.sql
+
+# crontab entry for scheduling backup daily at 23:59
+59 23 * * * /bin/bash /backup/backup.sh
